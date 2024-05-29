62ae6d769bb7c7d8e8381607ce8c3e8edf0d7505
ELIAS <elias.alasia.moreno@gmail.com>
2024-05-29 15:15:28 -0500

    actualizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { products as initialProducts } from './mocks/products.json'
 import { Products } from './components/Products.jsx'
 import { Header } from './components/Header.jsx'
-import { Footer } from './components/Footer.jsx'
+
 import { IS_DEVELOPMENT} from './config.js'
 import { useFilters } from './hooks/useFilters.js'
 import { Cart } from './components/Cart.jsx'
@@ -17,7 +17,7 @@ function App () {
       <Header />
       <Cart />
       <Products products={filteredProducts} />
-      {IS_DEVELOPMENT && <Footer />}
+      {IS_DEVELOPMENT }
     </CartProvider>
   )
 }
